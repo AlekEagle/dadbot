de067325d16ee6e07af1f38587152d3c6309d453
Alek Evans <alek@lugetron.com>
2019-06-17 14:31:13 -0600

    fixed toggle owner

--- cmds/toggleowner.js
+++ cmds/toggleowner.js
@@ -7,28 +7,20 @@ module.exports = {
 
     exec: (client, msg, args) => {
         var ownerID = args[0].replace(/(<@|!|>)/g, '')
-        if (!manager.gblacklist.users.includes(msg.author.id)) {
-            if (owners.isOwner(msg.author.id)) {
-                if (owners.isOwner(ownerID)) {
-                    owners.removeOwner(ownerID, msg.author.id).then(() => {
-                        msg.channel.createMessage('They are no longer an owner!');
-                    }, (err) => {
-                        if (err.message === 'functionCaller doesn\'t have permissions to affect other owner') msg.channel.createMessage('You do not have permission to edit this owner!'); else msg.channel.createMessage('An unknown error occurred while proforming this action.');console.error(err);
-                    });
-                }else if (!owners.isOwner(ownerID)) {
-                    owners.addOwner(ownerID, JSON.parse(args[1]), msg.author.id).then((newOwner) => {
-                        msg.channel.createMessage(`They are now an owner! And there admin is set to ${newOwner.adminOwner}`)
-                    });
-                }
-            }else {
-                msg.channel.createMessage('You need the permission `BOT_OWNER` to do this!');
+        if (owners.isOwner(msg.author.id)) {
+            if (owners.isOwner(ownerID)) {
+                owners.removeOwner(ownerID, msg.author.id).then(() => {
+                    msg.channel.createMessage('They are no longer an owner!');
+                }, (err) => {
+                    if (err.message === 'functionCaller doesn\'t have permissions to affect other owner') msg.channel.createMessage('You do not have permission to edit this owner!'); else msg.channel.createMessage('An unknown error occurred while proforming this action.');console.error(err);
+                });
+            }else if (!owners.isOwner(ownerID)) {
+                owners.addOwner(ownerID, JSON.parse(args[1]), msg.author.id).then((newOwner) => {
+                    msg.channel.createMessage(`They are now an owner! And there admin is set to ${newOwner.adminOwner}`)
+                });
             }
         }else {
-            msg.author.getDMChannel().then(chn => { 
-                chn.createMessage('You have been blacklisted from dad bot! If you think this is a mistake, please go here https://alekeagle.tk/discord and ask AlekEagle#0001 about this issue.').catch(() => {
-                    msg.channel.createMessage(`<@${msg.author.id}> You have been blacklisted from dad bot! If you think this is a mistake, please go here https://alekeagle.tk/discord and ask AlekEagle#0001 about this issue.`)
-                });
-            });
+            msg.channel.createMessage('You need the permission `BOT_OWNER` to do this!');
         }
     },
 
