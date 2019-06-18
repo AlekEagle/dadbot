9cdccf23247c0165cbd7da493ec12f79fb2e3100
Alek Evans <alek@lugetron.com>
2019-06-18 17:56:39 -0600

    made fix

--- cmds/listowners.js
+++ cmds/listowners.js
@@ -2,12 +2,12 @@
 
 const owners = require('../functions/getOwners');
 const shards = require('../functions/shardManager');
-let output = [];
 
 module.exports = {
     name: 'owners',
 
     exec: (client, msg, args) => {
+        let output = [];
         let ownerNames = owners.owners.users.map(o => shards.map(s => s.users.get(o.id))).reduce((a, b) => a.concat(b)).filter(a => a !== undefined).map(a => a).filter((i, o, s) => s.map(e => e.id).indexOf(i.id) == o)
         output.push(`NAME${' '.repeat(ownerNames.map(e => `${e.username}#${e.discriminator}`.length).sort((a, b) => a - b).reverse()[0] - 4)}|USERID${' '.repeat(ownerNames.map(e => e.id.length).sort((a, b) => a - b).reverse()[0] - 6)}|ADMIN`)
         output.push(`${'-'.repeat(ownerNames.map(e => `${e.username}#${e.discriminator}`.length).sort((a, b) => a - b).reverse()[0])}+${'-'.repeat(ownerNames.map(e => e.id.length).sort((a, b) => a - b).reverse()[0])}+-----`)
