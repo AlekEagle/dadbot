5e163a6ec64f1f2f6505892183dfc4e2b375d8c1
Alek Evans <alek@lugetron.com>
2019-06-21 16:38:51 -0600

    bruh yes

--- cmds/info.js
+++ cmds/info.js
@@ -4,6 +4,9 @@ let nums = require('../functions/numbers');
 let guildCount = require('../functions/getGuilds');
 let time = require('../functions/toReadableTime');
 let shards = require('../functions/shardManager');
+let util = require('util');
+let cpu = util.promisify(require('process-cpu-utilization').get);
+let memory = require('../functions/memoryUsage');
 
 module.exports = {
     name: 'info',
@@ -11,52 +14,65 @@ module.exports = {
     exec: (client, msg, args) => {
         msg.channel.sendTyping();
         guildCount().then(guilds => {
-            msg.channel.createMessage({
-                embed: {
-                    title: 'Info',
-                    fields: [
-                        {
-                            name: 'Commands ran',
-                            value: nums.cmdsRan,
-                            inline: true
-                        },
-                        {
-                            name: 'Messages Read',
-                            value: nums.msgsRead,
-                            inline: true
-                        },
-                        {
-                            name: 'Auto responses answered',
-                            value: nums.responses,
-                            inline: true
-                        },
-                        {
-                            name: 'Server count',
-                            value: guilds,
-                            inline: true
-                        },
-                        {
-                            name: 'User count',
-                            value: shards.map(s => s.users.size).reduce((a, b) => a + b, 0),
-                            inline: true
-                        },
-                        {
-                            name: 'Shards',
-                            value: nums.shardCount,
-                            inline: true
-                        },
-                        {
-                            name: 'Current Shard',
-                            value: msg.channel.guild.shard.id,
-                            inline: true
-                        },
-                        {
-                            name: 'Uptime',
-                            value: time(process.uptime())
-                        }
-                    ]
-                }
-            }).catch(err => {});
+            cpu(['%cpu']).then(data => {
+                msg.channel.createMessage({
+                    embed: {
+                        title: 'Info',
+                        fields: [
+                            {
+                                name: 'Commands ran',
+                                value: nums.cmdsRan,
+                                inline: true
+                            },
+                            {
+                                name: 'Messages Read',
+                                value: nums.msgsRead,
+                                inline: true
+                            },
+                            {
+                                name: 'Auto responses answered',
+                                value: nums.responses,
+                                inline: true
+                            },
+                            {
+                                name: 'Server count',
+                                value: guilds,
+                                inline: true
+                            },
+                            {
+                                name: 'User count',
+                                value: shards.map(s => s.users.size).reduce((a, b) => a + b, 0),
+                                inline: true
+                            },
+                            {
+                                name: 'Shards',
+                                value: nums.shardCount,
+                                inline: true
+                            },
+                            {
+                                name: 'Current Shard',
+                                value: msg.channel.guild.shard.id,
+                                inline: true
+                            },
+                            {
+                                name: 'RAM Usage',
+                                value: `${data['%cpu']}%`,
+                                inline: true
+                            },
+                            {
+                                name: 'Memory Usage',
+                                value: `${memory()}`,
+                                inline: true
+                            },
+                            {
+                                name: 'Uptime',
+                                value: time(process.uptime()),
+                                inline: true
+                            }
+                        ]
+                    }
+                }).catch(err => {});
+            });
         });
     },
 
---
+++ functions/memoryUsage.js
@@ -0,0 +1,64 @@
+'use strict';
+
+class KiloBytes {
+    raw() {
+        return process.memoryUsage().rss / 1024;
+    }
+    
+    stringify() {
+        let mem = process.memoryUsage().rss / 1024;
+        return `${mem.toString().slice(0,mem.toString().indexOf('.') + 3)}KB`;
+    }
+}
+
+class MegaBytes {
+    raw() {
+        return process.memoryUsage().rss / 1024 / 1024;
+    }
+    
+    stringify() {
+        let mem = process.memoryUsage().rss / 1024 / 1024;
+        return `${mem.toString().slice(0,mem.toString().indexOf('.') + 3)}MB`;
+    }
+}
+
+class GigaBytes {
+    raw() {
+        return process.memoryUsage().rss / 1024 / 1024 / 1024;
+    }
+    
+    stringify() {
+        let mem = process.memoryUsage().rss / 1024 / 1024 / 1024;
+        return `${mem.toString().slice(0,mem.toString().indexOf('.') + 3)}GB`;
+    }
+}
+
+class TeraBytes {
+    raw() {
+        return process.memoryUsage().rss / 1024 / 1024 / 1024 / 1024;
+    }
+    
+    stringify() {
+        let mem = process.memoryUsage().rss / 1024 / 1024 / 1024 / 1024;
+        return `${mem.toString().slice(0,mem.toString().indexOf('.') + 3)}GB`;
+    }
+}
+
+module.exports = () => {
+    let mem = process.memoryUsage().rss;
+    if((mem / 1024 / 1024 / 1024) > 1024) return `${(mem / 1024 / 1024 / 1024 / 1024).toString().slice(0, (mem / 1024 / 1024 / 1024 / 1024).toString().indexOf('.') + 3)}TB`;
+    else if ((mem / 1024 / 1024) > 1024) return `${(mem / 1024 / 1024 / 1024).toString().slice(0, (mem / 1024 / 1024 / 1024).toString().indexOf('.') + 3)}GB`;
+    else if ((mem / 1024) > 1024) return `${(mem / 1024 / 1024).toString().slice(0, (mem / 1024 / 1024).toString().indexOf('.') + 3)}MB`;
+    else if (mem > 1024) return `${(mem / 1024).toString().slice(0, (mem / 1024).toString().indexOf('.') + 3)}KB`;
+    else return `${mem}B`;
+}
+
+
+module.exports.KiloBytes = KiloBytes;
+module.exports.MegaBytes = MegaBytes;
+module.exports.GigaBytes = GigaBytes;
+module.exports.TeraBytes = TeraBytes;
+module.exports.KB = module.exports.KiloBytes;
+module.exports.MB = module.exports.MegaBytes;
+module.exports.GB = module.exports.GigaBytes;
+module.exports.TB = module.exports.TeraBytes;
