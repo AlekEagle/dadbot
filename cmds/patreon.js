a290d5d645ad4c3cf1f5d12619defec316eb55d5
Alek Evans <alek@lugetron.com>
2019-07-23 14:39:07 -0600

    mom

---
+++ cmds/patreon.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    name: 'patreon',
+
+    exec: (client, msg, args) => {
+        msg.channel.createMessage('Go support me and the person who made me by supporting them on patreon https://alekeagle.tk/patreon');
+    },
+
+    options: {
+        description: 'Patreon',
+        fullDescription: 'Patreon'
+    }
+}
