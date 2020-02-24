0a8065dc75b5306596def60f52fa3a7bbdc07916
Alek Evans <alek@lugetron.com>
2020-02-23 21:03:31 -0700

    fuck

---
+++ cmds/vsauce.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    name: 'vsauce',
+
+    exec: (client, msg, args) => {
+        msg.channel.createMessage('Join the official\* Vsauce discord server https://discord.gg/3P2zMTX  !!!!\n\n\n\n\n\*its not really official, i just wanted to say it was')
+    },
+
+    options: {
+        hidden: true
+    }
+}
