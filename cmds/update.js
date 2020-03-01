82598ca00424d9de8fbbf0331b846534d4f33a92
Alek Evans <alek@lugetron.com>
2020-03-01 15:30:02 -0700

    fuck

--- cmds/update.js
+++ cmds/update.js
@@ -13,31 +13,33 @@ module.exports = {
 
     exec: (client, msg, args) => {
         if (owners.isOwner(msg.author.id)) {
-            msg.channel.createMessage(`Updating...`).then(message => {
+            msg.channel.createMessage(`Updating <a:loading1:470030932775272469>`).then(message => {
                 exec('git pull', (err, stdout, stderr) => {
-                    for (let thing = 0; thing < nums.shardCount; thing++) {
-                        request({
-                            method: 'GET',
-                            url: `http://127.0.0.1:420${thing}/reloadcmds`
-                        }, (err, res, body) => {
-                            if (err) {
-                                console.error('can\'t connect to the other shards')
-                                console.error(err)
-                            }
-                        })
-                    }
-                    for (let thing = 0; thing < nums.shardCount; thing++) {
-                        request({
-                            method: 'GET',
-                            url: `http://127.0.0.1:420${thing}/reloadevts`
-                        }, (err, res, body) => {
-                            if (err) {
-                                console.error('can\'t connect to the other shards')
-                                console.error(err)
-                            }
-                        })
-                    }
-                    message.edit('Update complete.')
+                    setTimeout(() => {
+                        for (let thing = 0; thing < nums.shardCount; thing++) {
+                            request({
+                                method: 'GET',
+                                url: `http://127.0.0.1:420${thing}/reloadcmds`
+                            }, (err, res, body) => {
+                                if (err) {
+                                    console.error('can\'t connect to the other shards')
+                                    console.error(err)
+                                }
+                            })
+                        }
+                        for (let thing = 0; thing < nums.shardCount; thing++) {
+                            request({
+                                method: 'GET',
+                                url: `http://127.0.0.1:420${thing}/reloadevts`
+                            }, (err, res, body) => {
+                                if (err) {
+                                    console.error('can\'t connect to the other shards')
+                                    console.error(err)
+                                }
+                            })
+                        }
+                    }, 5000);
+                    message.edit('Update complete.');
                 });
             });
         }
