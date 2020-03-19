7d02baf9c735b680657726ff92cdeb7c70c5698a
Alek Evans <alek@lugetron.com>
2020-03-19 15:15:20 -0600

    bruh moment

--- cmds/complement.js
+++ cmds/complement.js
@@ -3,15 +3,15 @@
 const ms = require('ms');
 
 module.exports = {
-    name: 'complement',
+    name: 'compliment',
 
     exec: (client, msg, args) => {
         if (args.length <= 3) {
-            msg.channel.createMessage(`a complement ${args.length} word${args.length === 1 ? '' : 's'} long isn't very helpful.`);
+            msg.channel.createMessage(`a compliment ${args.length} word${args.length === 1 ? '' : 's'} long isn't very helpful.`);
         } else {
             client.createMessage('690286380122767481', {
                 embed: {
-                    title: 'New Complement',
+                    title: 'New compliment',
                     author: {
                         name: `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`,
                         icon_url: msg.author.dynamicAvatarURL('png', 512).split('?')[0]
@@ -50,8 +50,8 @@ module.exports = {
     },
 
     options: {
-        description: 'complement the creator about things',
-        fullDescription: 'Complement the creator about things in dad bot! Gets sent to the discord server!',
+        description: 'compliment the creator about things',
+        fullDescription: 'compliment the creator about things in dad bot! Gets sent to the discord server!',
         cooldown: ms('1 minute'),
         cooldownMessage: 'Hang on there buddy, you have a cooldown for this command, please wait at least 1 minutes before trying again.'
     }
