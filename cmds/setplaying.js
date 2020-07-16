dad153494696ad54230fd45816a5c337516953b2
Alek Evans <alek@lugetron.com>
2020-07-16 11:52:14 -0600

    fixed super secret command that no one can use

--- cmds/setplaying.js
+++ cmds/setplaying.js
@@ -9,27 +9,26 @@ module.exports = {
     exec: (client, msg, args) => {
         if (owners.isOwner(msg.author.id)) {
             var playing = '';
-            var text = args.slice(2).join(' ');
+            var text = args.slice(args[1].toLowerCase() === 'listening' ? 3 : 2).join(' ');
             var n = text.split(' | ')[1];
             text = text.split(' | ')[0];
-            switch(parseInt(args[1])) {
-
-            }
-            shards.map(s => s).forEach(s => {
-                s.editStatus(args[0], {
-                    name: text,
-                    type: playing,
-                    url: n
-                });
-            });
 
             let types = {
                 playing: '**Playing**',
                 streaming: '**Streaming**',
-                
+                'listening to': '**Listening To**',
+                watching: '**Watching**'
             }
 
-            msg.channel.createMessage('I am now ' + playing + ' ' + text);
+            shards.map(s => s).forEach(s => {
+                s.editStatus(args[0], {
+                    name: text,
+                    type: Object.keys(types).indexOf(args.slice(1, args[1].toLowerCase() === 'listening' ? 3 : 2).join(' ')),
+                    url: n
+                });
+            });
+
+            msg.channel.createMessage('I am now ' + types[args.slice(1, args[1].toLowerCase() === 'listening' ? 3 : 2).join(' ')] + ' ' + text);
 
         }else client.createMessage(msg.channel.id, 'You need the permission `BOT_OWNER` to use this command!')
         
