b4573e2d8e9fbf8e6f7d778915489b667c908f0a
Alek Evans <alek@lugetron.com>
2020-08-02 16:26:58 -0600

    whoops

--- cmds/embarrass.js
+++ cmds/embarrass.js
@@ -15,7 +15,7 @@ module.exports = {
             user = msg.member
         }
         var avatarURL = client.users.get(user.id).dynamicAvatarURL('png', 2048).split('?')[0]
-        if (permissionOverwrites(msg.channel, client.user.id, 'manageWebhooks') === null ? !msg.channel.guild.members.get(client.user.id).permission.has('manageWebhooks') : !permissionOverwrites(msg.channel, client.user.id, 'manageWebhooks')) {
+        if (permissionOverwrites(msg.channel, client.user.id, 'manageWebhooks') === null ? msg.channel.guild.members.get(client.user.id).permission.has('manageWebhooks') : permissionOverwrites(msg.channel, client.user.id, 'manageWebhooks')) {
             var embarrassingThing = lists.embarrassingThings[Math.floor(Math.random() * lists.embarrassingThings.length)]
             if (lists.embarrassingThings[0] === embarrassingThing || lists.embarrassingThings[1] === embarrassingThing) {
                 msg.channel.createMessage(`<@${user.id}> ${embarrassingThing}`);
