d57e8c9db582b8bf2378c25e24156cae54260267
Alek Evans <contact@alekeagle.com>
2020-09-08 08:46:32 -0600

    actually fixed it

--- cmds/info.js
+++ cmds/info.js
@@ -11,7 +11,7 @@ module.exports = {
 
   exec: (client, msg, args) => {
     msg.channel.sendTyping();
-    getCPUUsage((cpuusage) => {
+    cpuUsage((cpuusage) => {
       msg.channel
         .createMessage({
           embed: {
