08d6bb11d66d0ac6d934f717c79200b456c94b9a
Alek Evans <alek@lugetron.com>
2020-09-15 09:19:42 -0600

    ability to complain to discord about bot breaking

--- events/error.js
+++ events/error.js
@@ -1,9 +1,30 @@
 'use strict';
 
+const Sequelize = require('sequelize');
+class WSErrors extends Sequelize.Model {};
+WSErrors.init(
+  {
+    error: Sequelize.DataTypes.STRING(10485760),
+    admin: Sequelize.BOOLEAN,
+  },
+  { sequelize: _database }
+);
+WSErrors.sync({
+  force: false,
+})
+  .then(() => {
+    console.info("WSErrors synced to database successfully!");
+  })
+  .catch((err) => {
+    console.error("an error occured while proforming this operation", err);
+  });
+
+
 module.exports = {
     name: 'error',
 
     exec: (client, err) => {
-        console.error(err)
+        console.error(err);
+        WSErrors.create({error: require('util').inspect(err)});
     }
 }
