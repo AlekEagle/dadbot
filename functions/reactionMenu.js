fc31eebf14e29b35c9f96800ac048d008cd7fcd6
Alek Evans <alek@lugetron.com>
2020-11-12 15:40:58 -0700

    Updated settings command

--- functions/reactionMenu.js
+++ functions/reactionMenu.js
@@ -5,9 +5,10 @@ const Eris = require('eris'),
     ms = require('ms');
 
 class ReactionMenu {
-    constructor(client, channel, user, message, reactions, timeout) {
+    constructor(client, origMessage, user, message, reactions, timeout) {
         this.client = client;
-        this.channel = channel instanceof Eris.Channel ? channel : this.client.guilds.find(g => g.channels.get(channel))[0].channels.get(channel);
+        this.channel = origMessage.channel;
+        this.origMessage = origMessage;
         this.user = typeof user === 'string' ? [user] : user;
         this.state = 'default';
         this.messageChanging = true;
@@ -19,12 +20,13 @@ class ReactionMenu {
         }
         this.reactionHandlerInstance = this.reactionAddListener.bind(this);
         this.states.set('default', { message, reactions, emojis: new Map() });
-        this.channel.createMessage(message).then(message => {
+        this.channel.createMessage(typeof message === "function" ? message() : message).then(message => {
             this.message = message;
             this.client.on('messageReactionAdd', this.reactionHandlerInstance);
             this.addReactions(this.message, this.states.get(this.state).reactions).then(() => {
                 this.timeout = setTimeout(() => {
                     this.message.delete();
+                    this.origMessage.delete();
                     this.client.off('messageReactionAdd', this.reactionHandlerInstance);
                     this.message.channel.createMessage(`<@${this.user[0]}> menu timed-out due to inactivity!`).then(msg => {
                         setTimeout(() => msg.delete(), ms('5s'));
@@ -49,26 +51,34 @@ class ReactionMenu {
                     else addReaction(reactions[index]);
                 }, reject);
             }
+            if (reactions === undefined || reactions[index] === undefined) {
+                resolve();
+                return;
+            }
             addReaction(reactions[index]);
         });
     }
 
-    handleReaction(emoji, userID) {
+    async handleReaction(emoji, userID) {
         clearTimeout(this.timeout);
         this.timeout = setTimeout(() => {
             this.message.delete();
+            this.origMessage.delete();
             this.client.off('messageReactionAdd', this.reactionHandlerInstance);
             this.message.channel.createMessage(`<@${this.user[0]}> menu timed-out due to inactivity!`).then(msg => {
                 setTimeout(() => msg.delete(), ms('5s'));
             });
         }, ms(this.timeoutDur));
-        if (!this.states.has(this.state)) throw new Error('State does not exist');
-        if (this.states.get(this.state).emojis.has(emoji.id ? `${emoji.name}:${emoji.id}` : emoji.name)) this.states.get(this.state).emojis.get(emoji.id ? `${emoji.name}:${emoji.id}` : emoji.name)(this.message, userID);
+        if (!this.states.has(this.state)) throw new Error(`State '${this.state}' does not exist`);
+        if (this.states.get(this.state).emojis.has(emoji.id ? `${emoji.name}:${emoji.id}` : emoji.name)) await this.states.get(this.state).emojis.get(emoji.id ? `${emoji.name}:${emoji.id}` : emoji.name)(this.message, userID);
+        if (typeof this.states.get(this.state).message === 'function') {
+            this.message.edit((await this.states.get(this.state).message()));
+        }
     }
 
     addState(state, options) {
         if (!state) throw new Error('state is required');
-        this.states.set(state, { message: options ? (typeof options.message === 'function' ? options.message() : options.message) : null, reactions: options ? options.reactions : [], emojis: options ? (options.emojis instanceof Map ? emojis : typeof options.emojis !== "undefined" ? new Map(emojis) : new Map()) : new Map() });
+        this.states.set(state, { message: options ? options.message : null, reactions: options ? options.reactions : [], emojis: options ? (options.emojis instanceof Map ? emojis : typeof options.emojis !== "undefined" ? new Map(emojis) : new Map()) : new Map() });
     }
 
     addEmoji(state, emoji, action) {
@@ -82,18 +92,20 @@ class ReactionMenu {
         clearTimeout(this.timeout);
         this.client.off('messageReactionAdd', this.reactionHandlerInstance);
         this.message.delete();
+        this.origMessage.delete();
     }
 
     async setState(state) {
-        if (!this.states.has(state)) throw new Error('State does not exist');
+        if (!this.states.has(state)) throw new Error(`State '${state}' does not exist`);
         this.messageChanging = true;
         this.state = state;
         await this.message.removeReactions();
         if (this.states.get(this.state).message) {
-            await this.message.edit(this.states.get(this.state).message);
+            await this.message.edit(typeof this.states.get(this.state).message === "function" ? (await this.states.get(this.state).message()) : this.states.get(this.state).message);
         }
         await this.addReactions(this.message, this.states.get(this.state).reactions);
         this.messageChanging = false;
+        return;
     }
 
     addUser(userID) {
