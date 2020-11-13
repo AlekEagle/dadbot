c46f21ef5c1fc81df2ad2326bee37a0edefc4725
Alek Evans <alek@lugetron.com>
2020-11-13 12:31:50 -0700

    D!REBOOT WAS UNPROTECTED

--- cmds/reboot.js
+++ cmds/reboot.js
@@ -6,13 +6,14 @@ module.exports = {
     name: 'reboot',
 
     exec: (client, msg, args) => {
-        if (owners.isAdmin(msg.author.id)) {
-            client.createMessage(msg.channel.id, `Alright ${msg.author.username}! Imma go take a nap!`)
-            setTimeout(() => {
-                process.exit(0);
-            }, 100)
-        } else client.createMessage(msg.channel.id, 'You need the permission `BOT_ADMIN_OWNER` to use this command!')
-
+        owners.isAdmin(msg.author.id).then(owner => {
+            if (owner) {
+                client.createMessage(msg.channel.id, `Alright ${msg.author.username}! Imma go take a nap!`);
+                setTimeout(() => {
+                    process.exit(0);
+                }, 100)
+            } else client.createMessage(msg.channel.id, 'You need the permission `BOT_ADMIN_OWNER` to use this command!');
+        });
     },
 
     options: {
