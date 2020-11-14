89aa576c2788d2bcc3fb55727ebc92f9a76d03e3
Alek Evans <alek@lugetron.com>
2020-11-14 16:00:33 -0700

    restart all clusters simultaneously

--- cmds/reboot.js
+++ cmds/reboot.js
@@ -6,12 +6,27 @@ module.exports = {
     name: 'reboot',
 
     exec: (client, msg, args) => {
+        let i = 0;
+        function getData() {
+            return new Promise((resolve, reject) => {
+                if (i === Number(process.env.NODE_APP_INSTANCE)) {
+                    if (++i < Number(process.env.instances)) getData().then(resolve);
+                    else resolve();
+                } else {
+                    grafana.remoteEval(i, 'setTimeout(() => process.exit(0),500);').then(() => {
+                        if (++i < Number(process.env.instances)) getData().then(resolve);
+                        else resolve();
+                    }, reject);
+                }
+            });
+        }
         owners.isAdmin(msg.author.id).then(owner => {
             if (owner) {
-                client.createMessage(msg.channel.id, `Alright ${msg.author.username}! Imma go take a nap!`);
-                setTimeout(() => {
-                    process.exit(0);
-                }, 100)
+                getData().then(() => {
+                    client.createMessage(msg.channel.id, `Alright ${msg.author.username}! Imma go take a nap!`).then(() => {
+                        process.exit(0);
+                    });
+                });
             } else client.createMessage(msg.channel.id, 'You need the permission `BOT_ADMIN_OWNER` to use this command!');
         });
     },
