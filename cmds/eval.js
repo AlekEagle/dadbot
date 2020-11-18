db26bb1b51ad95e1b0bd5865f7719f6693b29e9f
Alek Evans <alek@lugetron.com>
2020-11-18 13:14:28 -0700

    penis

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,62 +1,87 @@
 "use strict";
 
-let owners = require("../functions/getOwners");
-let util = require("util");
+const owners = require("../functions/getOwners");
+const util = require("util");
+const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
+
+function evaluateSafe(code, args) {
+    const emitter = new (require("events"))();
+
+    function safeWrapSetter(fn, setter, clearer, ...args) {
+        const timeout = setter((...args) => {
+            try {
+                return fn(...args);
+            } catch (error) {
+                clearer(timeout);
+                emitter.emit("timeoutError", error, setter.name);
+            }
+        }, ...args)
+        return timeout;
+    }
+
+    const argss = Object.entries(Object.assign(args, {
+        "setTimeout": (fn, ...args) => { return safeWrapSetter(fn, setTimeout, clearTimeout, ...args) },
+        "setInterval": (fn, ...args) => { return safeWrapSetter(fn, setInterval, clearInterval, ...args) },
+        "setImmediate": (fn, ...args) => { return safeWrapSetter(fn, setImmediate, clearImmediate, ...args) }
+    }));
+
+    const evalFn = new AsyncFunction(...argss.map(arg => arg[0]), `${code}`);
+    Promise.resolve().then(() => evalFn(...argss.map(arg => arg[1]))).then(
+        thing => { emitter.emit("complete", thing, false) },
+        thing => { emitter.emit("complete", thing, true) }
+    );
+    return emitter;
+}
 
 module.exports = {
     name: "eval",
 
-    exec: (client, msg, args) => {
-        owners.isAdmin(msg.author.id).then(isAdmin => {
-            if (isAdmin) {
-                try {
-                    var evalCommand = args.join(" ");
-                    let evaluation = eval(evalCommand);
-                    if (typeof evaluation !== "string") {
-                        evaluation = util
-                            .inspect(evaluation)
-                            .replace(client.token, "(insert token here)");
+    exec: async (client, msg, args) => {
+        const isAdmin = await owners.isAdmin(msg.author.id);
+        if (isAdmin) {
+            let emitter = evaluateSafe(args.join(" ").replace(/(^(?:```(?:js\n))|(?:(?:\n?)```)$)/g, ''), {
+                "msg": msg,
+                "client": client
+            });
+            const evaledAt = Date.now();
+
+            emitter.once("complete", (result, isErr) => {
+                result = typeof result !== 'string' ? util.inspect(result) : result;
+                if (result.length > 1900) {
+                    const buffer = Buffer.from(result);
+                    if (buffer.length > 8388608) {
+                        msg.channel.createMessage('The output is too big for a file! You\'re outta\' luck!');
                     } else {
-                        evaluation = evaluation.replace(client.token, "(insert token here)");
+                        msg.channel.createMessage('The output is too big to fit in a message, here\'s a file instead!', { name: 'output.txt', file: buffer });
                     }
-                    if (evaluation.length > 2000) {
-                        if (evaluation.length > 8388608) {
-                            client.createMessage(
-                                msg.channel.id,
-                                "Output too large, even for a file! you're on your own this time!"
-                            );
-                            return;
-                        }
-                        client.createMessage(
-                            msg.channel.id,
-                            "Output too large, heres a file instead!",
-                            { file: Buffer.from(evaluation), name: "output.txt" }
-                        );
-                    } else {
-                        client.createMessage(msg.channel.id, evaluation);
-                    }
-                } catch (err) {
-                    client.createMessage(
-                        msg.channel.id,
-                        "OOF ERROR:\ninput: ```" +
-                        evalCommand +
-                        "``` output: ```" +
-                        err +
-                        "```"
-                    );
+                } else {
+                    msg.channel.createMessage(`${isErr ? 'An error occurred:\n' : ''}\`\`\`js\n${result}\n\`\`\``);
                 }
-            } else
-                client.createMessage(
-                    msg.channel.id,
-                    "You need the permission `BOT_ADMIN_OWNER` to use this command!"
-                );
-        });
+            });
+
+            emitter.on("timeoutError", (error, type) => {
+                error = typeof error !== "string" ? util.inspect(error) : error;
+                const extra = Date.now() - evaledAt >= 10000 ? `${msg.author.mention}! ` : "";
+
+                if (error.length > 1900) {
+                    const buffer = Buffer.from(error);
+                    if (buffer.length > 8388608) {
+                        msg.channel.createMessage(`${extra} There was an error in ${type} but it was too big for a file! You\'re outta\' luck, although I did clear the ${type} for you to prevent any more errors.`);
+                    } else {
+                        msg.channel.createMessage(`${extra} There was an error in ${type} but it was too big to fit in a message, here\'s a file instead! I've cleared the ${type} for you to prevent any more errors.`, { name: 'output.txt', file: buffer });
+                    }
+                } else {
+                    msg.channel.createMessage(`${extra} Error in ${type}: \`\`\`js\n${error}\n\`\`\`\nI've cleared the ${type} for you to prevent any more errors.`);
+                }
+            });
+        }
     },
 
     options: {
         hidden: true,
         fullDescription: "Evaluates code with a command (owner only)",
         aliases: ["evaluate", "ev"],
-        removeWhitespace: false,
+        removeWhitespace: true,
+        whitespaceSeparator: /(\s(?<!\n))/g
     },
 };
--- functions/logger.js
+++ functions/logger.js
@@ -1,5 +1,6 @@
 let chalk = require("chalk");
 let _console = { ...require('console') };
+let util = require('util');
 let date = function getNowDateTimeStr() {
     let now = new Date();
     let hour = now.getHours();
@@ -36,41 +37,41 @@ class Logger {
         if (logLevels.indexOf(this.logLevel) < logLevels.indexOf('LOG')) return;
         _console.log(
             `${chalk.blue.inverse(date())} ${chalk.inverse(`[LOG]`)} ${chalk.white(
-                thing
+                typeof thing !== 'string' ? util.inspect(thing) : thing
             )}`,
-            !args.length ? "" : args.join(' ')
+            !args.length ? "" : args.map(a => typeof a !== 'string' ? util.inspect(a) : a).join(' ')
         );
-        grafana.sendLog(thing).catch(() => { });
+        grafana.sendLog(typeof thing !== 'string' ? util.inspect(thing) : thing).catch(() => { });
     }
     warn(thing, ...args) {
         if (logLevels.indexOf(this.logLevel) < logLevels.indexOf('WARN')) return;
         _console.log(
             `${chalk.blue.inverse(date())} ${chalk
                 .rgb(255, 124, 43)
-                .inverse(`[WARN]`)} ${chalk.white(thing)}`,
-            !args.length ? "" : args.join(' ')
+                .inverse(`[WARN]`)} ${chalk.white(typeof thing !== 'string' ? util.inspect(thing) : thing)}`,
+            !args.length ? "" : args.map(a => typeof a !== 'string' ? util.inspect(a) : a).join(' ')
         );
-        grafana.sendError(thing).catch(() => { });
+        grafana.sendError(typeof thing !== 'string' ? util.inspect(thing) : thing).catch(() => { });
     }
     debug(thing, ...args) {
         if (logLevels.indexOf(this.logLevel) < logLevels.indexOf('DEBUG')) return;
         _console.log(
             `${chalk.blue.inverse(date())} ${chalk.grey.inverse(
                 `[DEBUG]`
-            )} ${chalk.white(thing)}`,
-            !args.length ? "" : args.join(' ')
+            )} ${chalk.white(typeof thing !== 'string' ? util.inspect(thing) : thing)}`,
+            !args.length ? "" : args.map(a => typeof a !== 'string' ? util.inspect(a) : a).join(' ')
         );
-        grafana.sendLog(thing).catch(() => { });
+        grafana.sendLog(typeof thing !== 'string' ? util.inspect(thing) : thing).catch(() => { });
     }
     info(thing, ...args) {
         if (logLevels.indexOf(this.logLevel) < logLevels.indexOf('INFO')) return;
         _console.log(
             `${chalk.blue.inverse(date())} ${chalk
                 .rgb(255, 255, 0)
-                .inverse(`[INFO]`)} ${chalk.white(thing)}`,
-            !args.length ? "" : args.join(' ')
+                .inverse(`[INFO]`)} ${chalk.white(typeof thing !== 'string' ? util.inspect(thing) : thing)}`,
+            !args.length ? "" : args.map(a => typeof a !== 'string' ? util.inspect(a) : a).join(' ')
         );
-        grafana.sendLog(thing).catch(() => { });
+        grafana.sendLog(typeof thing !== 'string' ? util.inspect(thing) : thing).catch(() => { });
     }
     error(thing, ...args) {
         if (logLevels.indexOf(this.logLevel) < logLevels.indexOf('ERROR')) return;
@@ -79,19 +80,19 @@ class Logger {
                 255,
                 0,
                 0
-            )(`[ERROR]`)} ${chalk.white(thing)}`,
-            !args.length ? "" : args.join(' ')
+            )(`[ERROR]`)} ${chalk.white(typeof thing !== 'string' ? util.inspect(thing) : thing)}`,
+            !args.length ? "" : args.map(a => typeof a !== 'string' ? util.inspect(a) : a).join(' ')
         );
-        grafana.sendError(thing).catch(() => { });
+        grafana.sendError(typeof thing !== 'string' ? util.inspect(thing) : thing).catch(() => { });
     }
     trace(thing, ...args) {
         _console.trace(
             `${chalk.blue.inverse(date())} ${chalk.inverse(`[TRACE]`)} ${chalk.white(
-                thing
+                typeof thing !== 'string' ? util.inspect(thing) : thing
             )}`,
-            !args.length ? "" : args.join(' ')
+            !args.length ? "" : args.map(a => typeof a !== 'string' ? util.inspect(a) : a).join(' ')
         );
-        grafana.sendLog(thing).catch(() => { });
+        grafana.sendLog(typeof thing !== 'string' ? util.inspect(thing) : thing).catch(() => { });
     }
 } Logger
 module.exports = (logLevel) => global.console = new Logger(logLevel);
