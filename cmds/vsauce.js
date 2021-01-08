ba8d9d9ffebaf92ef1d4fd6e6ebcd7f9b79cb39e
Alek Evans <contact@alekeagle.com>
2021-01-07 23:11:48 -0700

    peenids

--- cmds/vsauce.js
+++ cmds/vsauce.js
@@ -5,7 +5,7 @@ module.exports = {
 
   exec: (client, msg, args) => {
     msg.channel.createMessage(
-      'Join the official* Vsauce discord server https://discord.gg/gksj9bjBzw  !!!!\n\n\n\n\n*its not really official, i just wanted to say it was'
+      'Join the official* Vsauce discord server https://discord.gg/gMHemabCsF  !!!!\n\n\n\n\n*its not really official, i just wanted to say it was'
     );
   },
 
