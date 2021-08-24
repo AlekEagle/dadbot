23dbacd2df0a8330e901676eea584703fe513ced
Daniel Conley <himself@danii.dev>
2021-08-23 23:01:19 -0400

    Spoiler Im Responses

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import envConfig from './utils/dotenv';
 import {Client, Message, MessageContent, TextableChannel} from "eris";
+import { format } from 'path/posix';
 //import './utils/Sentry';
 
 async function typeSend(channel: TextableChannel, content: MessageContent) {
@@ -27,9 +28,10 @@ function wait(time: number) {
   return new Promise(timeup => setTimeout(timeup, time));
 }
 
-const IM_MATCH = /(im|i'm|i\s+am)\s+([\w\W]*)/i
+const IM_MATCH = /(im|i'm|i\s+am)\s+([\w\W]*)/i;
+const FORMAT_MATCH = /\*\*\*([\W\w]+)\*\*\*|\*\*([\W\w]+)\*\*|\*([\W\w]+)\*|```([\W\w]+)```|`([\W\w]+)`|_([\W\w]+)_|\|\|([\W\w]+)\|\|/ig;
 
-let client = new Client('get rekt lol');
+let client = new Client('ODA5MTEwOTg2MTUzNjU2MzQw.YCQVUw.sTJneA52rZuAg1sWiJd4psgi7NQ');
 client.on('messageCreate', onMessageCreate);
 
 async function onMessageCreate(message: Message) {
@@ -37,8 +39,42 @@ async function onMessageCreate(message: Message) {
 
   let match = message.content.match(IM_MATCH);
   if (match) {
-    typeSend(message.channel, `Hi ${match[2]}, I'm Dad!`);
+    let formatting = formats(message.content);
+    let spoiler = formatting[1]
+      .some(formatting => formatting.type == 7 && formatting.index + 2 >= match.index);
+    let fuck = formatting[0].match(IM_MATCH);
+
+    let outgoing = `Hi ${fuck[2]}, I'm Dad!`;
+    if (spoiler) outgoing = `||${outgoing}||`;
+    typeSend(message.channel, outgoing);
   }
+}
+
+type Yes = {type: number, content: string, index: number};
+
+function formats(raw: string): [string, Yes[]] {
+  let content = "";
+  let formatting: Yes[] = [];
+  let last = 0;
+
+  Array.from(raw.matchAll(FORMAT_MATCH))
+    .forEach(item => {
+      console.log(item);
+      let type = item.findIndex((item, index) => item && index != 0);
+      content += raw.substring(0, item.index);
+      last = item.index + item[0].length;
+      formatting.push({
+        "type": type,
+        "content": item[type],
+        "index": item.index + 1
+      });
+
+      let [innerContent, innerFormatting] = formats(item[type]);
+      content += innerContent;
+      formatting = [...formatting, ...innerFormatting];
+    });
+  content += raw.substr(last);
+  return [content, formatting];
 }
 
 client.connect();
