38dba41a6296469d2a2ef34a04afbede49e5a774
Alek Evans <github@alekeagle.com>
2021-11-21 21:16:01 -0700

    now with 20% less errors!

--- src/utils/CPU.ts
+++ src/utils/CPU.ts
@@ -3,7 +3,7 @@ import { exec } from 'node:child_process';
 export default function CPU(): Promise<number> {
   return new Promise((resolve, reject) => {
     exec(
-      `ps -auxf | egrep -v grep | egrep --color=none "${process.pid}.*\?.*Ssl"`,
+      `ps -auxf | egrep -v grep | egrep --color=none "${process.pid}.*\\?.*Ss?l"`,
       (e, stdout, stderr) => {
         if (e) reject([e, stderr]);
         else {
