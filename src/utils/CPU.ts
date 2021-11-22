dbf7c09bc0489e5675bee90d488cc01095508096
Alek Evans <github@alekeagle.com>
2021-11-21 21:31:21 -0700

    a

--- src/utils/CPU.ts
+++ src/utils/CPU.ts
@@ -2,14 +2,14 @@ import { exec } from 'node:child_process';
 
 export default function CPU(): Promise<number> {
   return new Promise((resolve, reject) => {
-    exec(
-      `ps -auxf | egrep -v grep | egrep --color=none "${process.pid}.*\\?.*Ss?l"`,
-      (e, stdout, stderr) => {
-        if (e) reject([e, stderr]);
-        else {
-          resolve(Number(stdout.split(/\s/g).filter(a => a !== '')[2]));
-        }
+    exec(`ps -auxf`, (e, stdout, stderr) => {
+      if (e) reject([e, stderr]);
+      else {
+        let line = stdout
+          .split('\n')
+          .filter(a => a.match(new RegExp(`${process.pid}.*\\?.*Ss?l`)))[0];
+        resolve(Number(line.split(/\s/g).filter(a => a !== '')[2]));
       }
-    );
+    });
   });
 }
