24595346f73c6dcc19f3ba76c2f849a61d61c4b9
Alek Evans <gpg@alekeagle.com>
2022-04-20 08:47:44 -0600

    fix error in mess with devs

--- src/commands/DevComputers.ts
+++ src/commands/DevComputers.ts
@@ -1,31 +1,35 @@
-import fetch from "node-fetch";
-import { CommandModule } from "../types";
+import fetch from 'node-fetch';
+import { CommandModule } from '../types';
 
 const DevComputers: CommandModule = {
-  name: "devcomputers",
+  name: 'devcomputers',
 
   async handler(client, msg, args) {
-    const res = await fetch("https://dumb-alek.alekeagle.com/clients", {
-      method: "GET",
+    const res = await fetch('https://dumb-alek.alekeagle.com/clients', {
+      method: 'GET',
       headers: {
-        Authorization: process.env.messWithAlekToken,
-      },
+        Authorization: process.env.messWithAlekToken
+      }
     });
     const json = await res.json();
+
+    if (json.length === 0) {
+      return 'There are no computers connected. Try again later.';
+    }
 
     return `These computers are currently connected: \`\`\`\n${json
       .map(
         (x: { username: string; computer: string }) =>
           `${x.username}'s computer named ${x.computer}`
       )
-      .join("\n")}\n\`\`\``;
+      .join('\n')}\n\`\`\``;
   },
 
   options: {
-    description: "Get a list of all active computers.",
-    fullDescription: "Get a list of all active computers.",
-    usage: "",
-  },
+    description: 'Get a list of all active computers.',
+    fullDescription: 'Get a list of all active computers.',
+    usage: ''
+  }
 };
 
 export default DevComputers;
--- src/commands/MessWithDevs.ts
+++ src/commands/MessWithDevs.ts
@@ -16,10 +16,10 @@ const MessWithDevs: CommandModule = {
       },
       body: args.join(' ')
     });
-    const json = await res.json();
 
     switch (res.status) {
       case 200:
+        const json = await res.json();
         return `Sent! It will be displayed on \`\`\`\n${json
           .map(
             (x: { username: string; computer: string }) =>
@@ -29,7 +29,7 @@ const MessWithDevs: CommandModule = {
       case 502:
         return "I'm sorry, I couldn't connect to the server. Please try again later.";
       case 503:
-        return "He isn't on any of his computers. Try again later.";
+        return 'There are no computers connected. Try again later.';
       default:
         return 'Uhhh... something went wrong. Try again later.';
     }
