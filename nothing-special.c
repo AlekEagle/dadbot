9135e13c67f5e7b8351912cdb06eb350115986b5
Alek Evans <contact@alekeagle.com>
2022-05-06 07:27:58 -0700

    nothing special

---
+++ nothing-special.c
@@ -0,0 +1,1 @@
+main(){int i=0;char*c = "(5((((`+((((f(H+(P7(d(([)(/(`G(X)7d+(.X+d)X+d(_(d.GXA`gg/d(dggI7(dgg`+(dg7^)(dgKE((dGD/((d/K)(((((((((0X)(((7F(<(XK/H7(d`)`)(.D(F(H(G(/(D`7X(HOg+6(`YgX;(fd7d7//d+7X[+GHKgdX7gg77fcggYKgfg/";while(i++<1122)printf(i%34?(c[i/6]-40&1<<i%6)?"XX":"  ":"\n");}
