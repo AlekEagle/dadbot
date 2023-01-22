b7695b1afc9df218e4e4b58e814077d6c0357ba6
Alek Evans <github@alekeagle.com>
2023-01-21 15:27:11 -0900

    patreon stuff and a bit more command handler
    Co-authored-by: Daniel <himself@danii.dev>

--- src/utils/CommandHandler/OptionBuilder.ts
+++ src/utils/CommandHandler/OptionBuilder.ts
@@ -1,3 +1,5 @@
+import { User, Member } from "oceanic.js";
+
 export type OptionChoice<T> = {
   name: string;
   value: T;
@@ -8,6 +10,8 @@ export type Options<P extends { [option: string]: OptionParameter }> = {
     ? string
     : P[K] extends { type: 4 }
     ? number
+    : P[K] extends { type: 6 }
+    ? User | Member
     : never;
 };
 
@@ -18,14 +22,29 @@ export type OptionParameter = (
   | { type: 4 }
   | { type: 4; choices: OptionChoice<number>[] }
   | { type: 4; max_value?: number; min_value?: number }
+  | { type: 6 }
 ) & { description: string; required: boolean };
 
 export namespace OptionBuilder {
   export function String(
     description: string,
-    required: boolean,
-    options: any
+    required: boolean = true,
+    options:
+      | { choices: OptionChoice<string>[] }
+      | { min_length: number; max_length: number }
+      | {} = {}
   ): OptionParameter {
-    return { type: 3, description, required };
+    return { type: 3, description, required, ...options };
+  }
+
+  export function Number(
+    description: string,
+    required: boolean = true,
+    options:
+      | { choices: OptionChoice<number>[] }
+      | { min_value?: number; max_value?: number }
+      | {} = {}
+  ): OptionParameter {
+    return { type: 4, description, required, ...options };
   }
 }
