63024977954f9ea88c600b8ad8f2fe4bf20c6820
Alek Evans <github@alekeagle.com>
2026-03-07 16:58:49 -0900

    lol

--- src/index.ts
+++ src/index.ts
@@ -248,7 +248,9 @@ if (!process.env.CLUSTERS || !process.env.CLUSTER_ID) {
   // This event listener will be removed when the ready event is emitted.
   function logShardStatus(shard: number) {
     console.log(
-      chalk.green(` Shard ${shard} of ${shards.thisCluster.count} is ready.`),
+      chalk.green(
+        ` Shard ${shard} of ${shards.thisCluster.start}-${shards.thisCluster.end} is ready.`,
+      ),
     );
   }
 
